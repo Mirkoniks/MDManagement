e80c073f97cf3240a4e98fae8b5ffd7470d90797
mirkoniks <prof.mirro@gmail.com>
2020-04-21 23:59:49 +0300

    Added dependencies and some packages

---
+++ MDManagement.Common/DataValidations.cs
@@ -0,0 +1,45 @@
+﻿namespace MDManagement.Common
+{
+    public static class DataValidations
+    {
+        public static class Adress
+        {
+            public const int AddressMaxValue = 150;
+        }
+
+        public static class Company
+        {
+            public const int CompanyCodeMaxValue = 15;
+            public const int NameMaxValue = 30;
+        }
+
+        public static class ApplicarionUser
+        {
+            public const int NameMaxValue = 30;
+            public const string SalaryDeciamlSecifications = "decimal (18,4)";
+        }
+
+        public static class Department
+        {
+            public const int NameMaxValue = 30;
+        }
+
+        public static class JobTitle
+        {
+            public const int NameMaxValue = 30;
+            public const int DescriptionMaxValue = 100;
+        }
+
+        public static class Project
+        {
+            public const int NameMaxValue = 30;
+            public const int DescriptionMaxValue = 100;
+        }
+
+        public static class Town
+        {
+            public const int NameMaxValue = 30;
+            public const int PostCodeMaxValue = 4;
+        }
+    }
+}
